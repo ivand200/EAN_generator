7cdc3eaa3d9465cf9d59d6cac0a370d525ae7efe
ivand1988 <ivand1988@outlook.com>
2021-12-10 15:37:57 +0300

    added svg

--- ean13.py
+++ ean13.py
@@ -2,9 +2,19 @@ import barcode
 from barcode.writer import ImageWriter
 import random
 
+def png_code():
+    for i in range(10):
+        code = random.randint(111111111111,999999999999)
+        ean = barcode.get('ean13', f'{str(code)}', writer=ImageWriter())
+        filename = ean.save(f'ean13-{i}')
+        filename
 
-for i in range(10):
-    code = random.randint(111111111111,999999999999)
-    ean = barcode.get('ean13', f'{str(code)}', writer=ImageWriter())
-    filename = ean.save(f'ean13-{i}')
+
+def svg_code():
+    ean = barcode.get('ean13', '123456789012')
+    ean.get_fullcode()
+    options = dict(compress=True)
+    filename = ean.save('ean13-svg', options)
     filename
+
+svg_code()
